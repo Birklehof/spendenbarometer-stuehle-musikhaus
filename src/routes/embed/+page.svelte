0bdfdc2c0d3aca98472b4ed214dbe42db2a48d71
Paul Maier <contact@paulmaier.online>
2025-11-21 03:46:52 +0100

    preview page

--- src/routes/embed/+page.svelte
+++ src/routes/embed/+page.svelte
@@ -1,3 +1,10 @@
-<div class="container">
-<iframe class="responsive-iframe" src="/" width="660" height="310"></iframe>
+<div class="flex h-screen w-screen flex-row justify-center">
+	<div class="flex flex-col gap-2 p-2 w-full max-w-xl">
+		<h1 class="text-2xl">Heading</h1>
+    <p>Some text above the iframe</p>
+		<div class="container">
+			<iframe class="responsive-iframe" src="/" width="300" height="300"></iframe>
+		</div>
+    <p>Some text below the iframe</p>
+	</div>
 </div>
